db35fb10ced21f720debdc575fdf7088e22680d9
DaPSL <145846855+IAmThePSL@users.noreply.github.com>
2025-01-02 22:56:42 +0100

    Update token.h

--- include/token.h
+++ include/token.h
@@ -2,61 +2,20 @@
 #define TOKEN_H
 
 #include <stddef.h>
-
-// Token types enumeration
-typedef enum {
-    TOKEN_INT,
-    TOKEN_STR,
-    TOKEN_BOOL,
-    TOKEN_LET,
-    TOKEN_CONST,
-    TOKEN_FUNCTION,
-    TOKEN_IF,
-    TOKEN_ELSE,
-    TOKEN_FOR,
-    TOKEN_WHILE,
-    TOKEN_RETURN,
-    TOKEN_CLASS,
-    TOKEN_IMPORT,
-    TOKEN_TRUE,
-    TOKEN_FALSE,
-    TOKEN_NULL,
-
-    TOKEN_PLUS,
-    TOKEN_MINUS,
-    TOKEN_ASTERISK,
-    TOKEN_SLASH,
-    TOKEN_PERCENT,
-    TOKEN_LPAREN,
-    TOKEN_RPAREN,
-    TOKEN_LBRACE,
-    TOKEN_RBRACE,
-    TOKEN_LBRACKET,
-    TOKEN_RBRACKET,
-    TOKEN_ASSIGN,
-
-    TOKEN_IDENTIFIER,
-    TOKEN_INTEGER_LITERAL,
-    TOKEN_STRING_LITERAL,
-
-    TOKEN_OPERATOR, // do we actually need this?
-    TOKEN_DELIMITER, // same goes for this one...
-
-    TOKEN_ERROR,
-    TOKEN_EOF,
-    TOKEN_SEMICOLON,
-} TokenType;
+#include "token_types.h"
 
 // Token structure
 typedef struct {
-    TokenType type;
-    char *lexeme;
-    int line;
-    int column;
+    TokenType type;     // Type of the token
+    char *lexeme;       // The string representation of the token
+    int line;           // Line number where the token appears
+    int column;         // Column number where the token starts
 } Token;
 
-// Token API
-extern Token *create_token(TokenType type, const char *lexeme, int line, int column);
-extern void destroy_token(Token *token);
+// Function to create a new token
+Token *create_token(TokenType type, const char *lexeme, int line, int column);
+
+// Function to destroy a token and free its memory
+void destroy_token(Token *token);
 
 #endif // TOKEN_H
