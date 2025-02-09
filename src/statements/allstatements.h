fd34f4c2e1dd237e2b89252c51808184b3f96ca6
DaPSL <145846855+IAmThePSL@users.noreply.github.com>
2025-02-09 22:10:10 +0100

    Delete src/statements/allstatements.h

--- src/statements/allstatements.h
+++
@@ -1,9 +0,0 @@
-#ifndef ALLSTATEMENTS_H
-#define ALLSTATEMENTS_H
-
-#include "ifstatement.h"
-#include "printstatement.h"
-#include "whilestatement.h"
-#include "variabledeclaration.h"
-
-#endif // ALLSTATEMENTS_H
