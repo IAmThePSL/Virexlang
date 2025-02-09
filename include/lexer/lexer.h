5ce08f240a0d90e42e84c089c5f78e2ee511f1f6
DaPSL <145846855+IAmThePSL@users.noreply.github.com>
2025-02-09 22:16:15 +0100

    Create lexer.h

---
+++ include/lexer/lexer.h
@@ -0,0 +1,22 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <stdio.h>
+#include "token.h"
+
+// Lexer structure definition
+typedef struct Lexer{
+		FILE *file;
+		FILE *input_stream;
+		int line;
+		int column;
+		int current_char;
+}Lexer;
+
+// Lexer API
+Lexer *create_lexer(FILE *file);
+void destroy_lexer(Lexer *lexer);
+Token *lex_next_token(Lexer *lexer);
+void lex_unget_token(Lexer *lexer, Token *token);
+
+#endif // LEXER_H
