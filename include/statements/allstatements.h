ca33793bd152099d1f98abb84dad98de1ba87b01
DaPSL <145846855+IAmThePSL@users.noreply.github.com>
2025-02-09 22:18:06 +0100

    Create allstatements.h

---
+++ include/statements/allstatements.h
@@ -0,0 +1,9 @@
+#ifndef ALLSTATEMENTS_H
+#define ALLSTATEMENTS_H
+
+#include "ifstatement.h"
+#include "printstatement.h"
+#include "whilestatement.h"
+#include "variabledeclaration.h"
+
+#endif // ALLSTATEMENTS_H
