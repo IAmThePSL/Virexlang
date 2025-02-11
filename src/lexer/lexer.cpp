d335c57ead2919f4d7903f6906ee517d6149ee67
DaPSL <145846855+IAmThePSL@users.noreply.github.com>
2025-02-11 10:45:12 +0100

    Update and rename lexer.c to lexer.cpp

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -1,11 +1,11 @@
 #include "../../include/lexer/token.h"
 #include "../../include/lexer/token_types.h"
 #include "../../include/lexer/lexer.h"
-#include "../../include/parser/malloc.h" // includes the safe_malloc function
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "../../include/parser/safe_alloc.h" // includes the safe_malloc function
+#include <cctype>
+#include <iostream>
+#include <fstream>
+#include <cstring>
 
 #define MAX_LEXEME_LENGTH 256
 #define KEYWORD_COUNT 25
@@ -14,80 +14,71 @@
 int evaluate_expression(Token *condition);
 int unget_buffer_index = 0;
 
-Token unget_buffer[MAX_UNGET_TOKENS];
+Token* unget_buffer[MAX_UNGET_TOKENS];
 
-void lex_unget_token(Lexer *lexer, Token *token){
+void lex_unget_token(Lexer* lexer, Token* token) {
     if (unget_buffer_index < MAX_UNGET_TOKENS) {
-        unget_buffer[unget_buffer_index++] = *token;
+        unget_buffer[unget_buffer_index++] = token;
     } else {
-        fprintf(stderr, "Error: unget buffer overflow\n");
+        std::cerr << "Error: unget buffer overflow" << std::endl;
     }
 }
 
-
-// This is defined in the lexer.h header file
-// So, no need to define it here again.
-// typedef struct Lexer {
-//     FILE *file;
-//     int current_char;
-//     int line;
-//     int column;
-// } Lexer;
-
 // Hash table for keywords
-typedef struct Keyword {
-    const char *word;
+struct Keyword {
+    const char* word;
     TokenType type;
-} Keyword;
+};
 
 const Keyword keywords[KEYWORD_COUNT] = {
-    {"int", TOKEN_INT},       {"str", TOKEN_STR},     {"bool", TOKEN_BOOL},
-    {"let", TOKEN_LET},       {"const", TOKEN_CONST}, {"function", TOKEN_FUNCTION},
-    {"if", TOKEN_IF},         {"else", TOKEN_ELSE},   {"for", TOKEN_FOR},
-    {"while", TOKEN_WHILE},   {"return", TOKEN_RETURN}, 
-    {"class", TOKEN_CLASS},   {"import", TOKEN_IMPORT},
-    {"true", TOKEN_TRUE},     {"false", TOKEN_FALSE}, {"null", TOKEN_NULL},
-    {"print", TOKEN_PRINT}};
+    {"int", TOKEN_INT}, {"str", TOKEN_STR}, {"bool", TOKEN_BOOL},
+    {"let", TOKEN_LET}, {"const", TOKEN_CONST}, {"function", TOKEN_FUNCTION},
+    {"if", TOKEN_IF}, {"else", TOKEN_ELSE}, {"for", TOKEN_FOR},
+    {"while", TOKEN_WHILE}, {"return", TOKEN_RETURN}, 
+    {"class", TOKEN_CLASS}, {"import", TOKEN_IMPORT},
+    {"true", TOKEN_TRUE}, {"false", TOKEN_FALSE}, {"null", TOKEN_NULL},
+    {"print", TOKEN_PRINT}
+};
 
 // Create a new lexer
-Lexer *create_lexer(FILE *file) {
-    Lexer *lexer = (Lexer *)safe_malloc(sizeof(Lexer));
-    lexer->file = file;
-    lexer->current_char = fgetc(file);
+Lexer* create_lexer(std::ifstream& file) {
+    Lexer* lexer = (Lexer*)safe_malloc(sizeof(Lexer));
+    lexer->file = &file;
+    lexer->current_char = file.get();
     lexer->line = 1;
     lexer->column = 1;
     return lexer;
 }
 
 // Destroy the lexer
-void destroy_lexer(Lexer *lexer) {
+void destroy_lexer(Lexer* lexer) {
     if (lexer) {
-        fclose(lexer->file);
-        free(lexer);
+        delete lexer->file;
+        delete lexer;
     }
 }
 
 // Advance to the next character
-int lex_advance(Lexer *lexer) {
+int lex_advance(Lexer* lexer) {
     if (lexer->current_char == '\n') {
         lexer->line++;
         lexer->column = 1;
     } else {
         lexer->column++;
     }
-    lexer->current_char = fgetc(lexer->file);
+    lexer->current_char = lexer->file->get();
     return lexer->current_char;
 }
 
 // Skip whitespace
-void skip_whitespace(Lexer *lexer) {
-    while (isspace(lexer->current_char)) {
+void skip_whitespace(Lexer* lexer) {
+    while (std::isspace(lexer->current_char)) {
         lex_advance(lexer);
     }
 }
 
 // Skip comments
-void skip_comments(Lexer *lexer) {
+void skip_comments(Lexer* lexer) {
     if (lexer->current_char == '/') {
         lex_advance(lexer); // Advance past first '/'
         if (lexer->current_char == '/') {
@@ -117,16 +108,16 @@ void skip_comments(Lexer *lexer) {
 }
 
 // Keyword lookup
-TokenType lookup_keyword(const char *lexeme) {
+TokenType lookup_keyword(const std::string& lexeme) {
     for (int i = 0; i < KEYWORD_COUNT; i++) {
-        if (strcmp(lexeme, keywords[i].word) == 0) {
+        if (lexeme == keywords[i].word) {
             return keywords[i].type;
         }
     }
     return TOKEN_IDENTIFIER;
 }
 
-Token *lex_identifier_or_keyword(Lexer *lexer) {
+Token* lex_identifier_or_keyword(Lexer* lexer) {
     char lexeme[MAX_LEXEME_LENGTH] = {0};
     int length = 0;
     int start_column = lexer->column;
@@ -136,12 +127,12 @@ Token *lex_identifier_or_keyword(Lexer *lexer) {
     lex_advance(lexer);
 
     // Read the rest of the identifier
-    while (isalnum(lexer->current_char) || lexer->current_char == '_') {
+    while (std::isalnum(lexer->current_char) || lexer->current_char == '_') {
         if (length < MAX_LEXEME_LENGTH - 1) {
             lexeme[length++] = lexer->current_char;
             lex_advance(lexer);
         } else {
-            fprintf(stderr, "Error: Identifier too long\n");
+            std::cerr << "Error: Identifier too long" << std::endl;
             break;
         }
     }
@@ -151,12 +142,12 @@ Token *lex_identifier_or_keyword(Lexer *lexer) {
     return create_token(type, lexeme, lexer->line, start_column);
 }
 
-Token *lex_number(Lexer *lexer) {
+Token* lex_number(Lexer* lexer) {
     char lexeme[MAX_LEXEME_LENGTH] = {0};
     int length = 0;
     int start_column = lexer->column;
 
-    while (isdigit(lexer->current_char)) {
+    while (std::isdigit(lexer->current_char)) {
         if (length < MAX_LEXEME_LENGTH - 1) {
             lexeme[length++] = lexer->current_char;
         }
@@ -168,7 +159,7 @@ Token *lex_number(Lexer *lexer) {
 }
 
 // Lex strings
-Token *lex_string(Lexer *lexer) {
+Token* lex_string(Lexer* lexer) {
     char lexeme[MAX_LEXEME_LENGTH] = {0};
     int length = 0;
     int start_column = lexer->column;
@@ -187,16 +178,17 @@ Token *lex_string(Lexer *lexer) {
                 case '"': lexeme[length++] = '"'; break;
                 case '\\': lexeme[length++] = '\\'; break;
                 default:
-                    fprintf(stderr, "Error: Unknown escape sequence '\\%c' at line %d, column %d\n",
-                            lexer->current_char, lexer->line, lexer->column);
+                    std::cerr << "Error: Unknown escape sequence '\\"
+                              << lexer->current_char << "' at line "
+                              << lexer->line << ", column " << lexer->column << std::endl;
                     lexeme[length++] = lexer->current_char; // Include the unknown escape sequence as-is
                     break;
             }
         } else if (length < MAX_LEXEME_LENGTH - 1) {
             lexeme[length++] = lexer->current_char;
         } else {
-            fprintf(stderr, "Error: String literal exceeds max length at line %d, column %d\n",
-                    lexer->line, start_column);
+            std::cerr << "Error: String literal exceeds max length at line "
+                      << lexer->line << ", column " << start_column << std::endl;
             break;
         }
         lex_advance(lexer);
@@ -207,16 +199,16 @@ Token *lex_string(Lexer *lexer) {
         lexeme[length] = '\0';
         return create_token(TOKEN_STRING_LITERAL, lexeme, lexer->line, start_column);
     } else {
-        fprintf(stderr, "Error: Unterminated string literal at line %d, column %d\n",
-                lexer->line, start_column);
+        std::cerr << "Error: Unterminated string literal at line "
+                  << lexer->line << ", column " << start_column << std::endl;
         return create_token(TOKEN_ERROR, "Unterminated string", lexer->line, start_column);
     }
 }
 
 // Lex next token
-Token *lex_next_token(Lexer *lexer) {
+Token* lex_next_token(Lexer* lexer) {
     if (!lexer || !lexer->file) {
-        return NULL;
+        return nullptr;
     }
 
     skip_whitespace(lexer);
@@ -227,8 +219,7 @@ Token *lex_next_token(Lexer *lexer) {
 
     // Handle comments
     if (lexer->current_char == '/') {
-        int next_char = fgetc(lexer->file);
-        ungetc(next_char, lexer->file);
+        int next_char = lexer->file->peek();
         if (next_char == '/' || next_char == '*') {
             skip_comments(lexer);
             return lex_next_token(lexer);
@@ -238,12 +229,12 @@ Token *lex_next_token(Lexer *lexer) {
     int start_column = lexer->column;
 
     // Handle identifiers and keywords
-    if (isalpha(lexer->current_char) || lexer->current_char == '_') {
+    if (std::isalpha(lexer->current_char) || lexer->current_char == '_') {
         return lex_identifier_or_keyword(lexer);
     }
 
     // Handle numbers
-    if (isdigit(lexer->current_char)) {
+    if (std::isdigit(lexer->current_char)) {
         return lex_number(lexer);
     }
 
@@ -254,8 +245,8 @@ Token *lex_next_token(Lexer *lexer) {
 
     // Single-character tokens
     char current_char = lexer->current_char;
-    Token *token = NULL;
-    
+    Token* token = nullptr;
+
     switch (current_char) {
         case '+': 
             lex_advance(lexer); 
@@ -281,10 +272,6 @@ Token *lex_next_token(Lexer *lexer) {
             lex_advance(lexer); 
             token = create_token(TOKEN_ASSIGN, "=", lexer->line, start_column);
             break;
-        case ';': 
-            lex_advance(lexer); 
-            token = create_token(TOKEN_SEMICOLON, ";", lexer->line, start_column);
-            break;
         case '(': 
             lex_advance(lexer); 
             token = create_token(TOKEN_LPAREN, "(", lexer->line, start_column);
@@ -301,17 +288,29 @@ Token *lex_next_token(Lexer *lexer) {
             lex_advance(lexer); 
             token = create_token(TOKEN_RBRACE, "}", lexer->line, start_column);
             break;
+        case '[': 
+            lex_advance(lexer); 
+            token = create_token(TOKEN_LBRACKET, "[", lexer->line, start_column);
+            break;
+        case ']': 
+            lex_advance(lexer); 
+            token = create_token(TOKEN_RBRACKET, "]", lexer->line, start_column);
+            break;
+        case ',': 
+            lex_advance(lexer); 
+            token = create_token(TOKEN_COMMA, ",", lexer->line, start_column);
+            break;
+        case ';': 
+            lex_advance(lexer); 
+            token = create_token(TOKEN_SEMICOLON, ";", lexer->line, start_column);
+            break;
         default:
-            fprintf(stderr, "Error: Unknown character '%c' at line %d, column %d\n",
-                    current_char, lexer->line, lexer->column);
+            std::cerr << "Error: Unrecognized character '" << current_char
+                      << "' at line " << lexer->line << ", column " << lexer->column << std::endl;
             lex_advance(lexer);
-            token = create_token(TOKEN_ERROR, "Unknown character", lexer->line, start_column);
+            token = create_token(TOKEN_ERROR, "Unknown", lexer->line, start_column);
             break;
     }
 
-    if (unget_buffer_index > 0) {
-        return &unget_buffer[--unget_buffer_index];
-    }
-    destroy_token(token); // Ensure token is destroyed
     return token;
 }
