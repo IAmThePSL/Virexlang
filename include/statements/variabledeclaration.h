08e35db5a4b915edeb35c4a6cd239171ca508639
DaPSL <145846855+IAmThePSL@users.noreply.github.com>
2025-02-11 11:36:56 +0100

    Update variabledeclaration.h

--- include/statements/variabledeclaration.h
+++ include/statements/variabledeclaration.h
@@ -2,11 +2,15 @@
 #define VARIABLEDECLARATION_H
 
 #include "../../include/parser/parser.h"
-#include <ctype.h>
+#include <cctype>
+#include <string>
 
-void handle_variable_declaration(Lexer *lexer, int is_const);
+class VariableDeclaration {
+public:
+    static void handleVariableDeclaration(Lexer &lexer, bool isConst);
 
-// Function to trim leading and trailing whitespaces
-char *trimWhitespace(char *str);
+    // Function to trim leading and trailing whitespaces
+    static std::string trimWhitespace(const std::string &str);
+};
 
 #endif // VARIABLEDECLARATION_H
